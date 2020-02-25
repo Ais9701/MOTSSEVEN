b4511077d799f3c25df326fe9ba4d9d03719dd9c
Ais9701 <61444536+Ais9701@users.noreply.github.com>
2020-02-25 13:38:22 +0500

    Add files via upload

--- FormMenu.Designer.cs
+++ FormMenu.Designer.cs
@@ -29,6 +29,8 @@
         private void InitializeComponent()
         {
             this.backbutton = new System.Windows.Forms.Button();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
             this.SuspendLayout();
             // 
             // backbutton
@@ -41,15 +43,26 @@
             this.backbutton.UseVisualStyleBackColor = true;
             this.backbutton.Click += new System.EventHandler(this.backbutton_Click);
             // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Image = global::ProbaApp.Properties.Resources.Распечатать;
+            this.pictureBox1.Location = new System.Drawing.Point(30, 61);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(456, 355);
+            this.pictureBox1.TabIndex = 1;
+            this.pictureBox1.TabStop = false;
+            // 
             // FormMenu
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.Color.Thistle;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.pictureBox1);
             this.Controls.Add(this.backbutton);
             this.Name = "FormMenu";
             this.Text = "Меню";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
             this.ResumeLayout(false);
 
         }
@@ -57,5 +70,6 @@
         #endregion
 
         private System.Windows.Forms.Button backbutton;
+        private System.Windows.Forms.PictureBox pictureBox1;
     }
 }
